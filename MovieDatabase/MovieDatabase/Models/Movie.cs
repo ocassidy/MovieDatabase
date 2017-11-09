c72de7a0d1395a9d11a9ba1c97c9a1ba10cc61b4
Ongsay <32300234+Ongsay@users.noreply.github.com>
2017-11-09 13:57:29 +0000

    Database
    I have no idea how to do this properly so I'm just uploading what I got so far into this branch.

---
+++ MovieDatabase/MovieDatabase/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieDatabase.Models
+{
+    
+    public class Movie
+    {
+        // declare the movie properties and a constructor to initialise a blank movie
+    
+    }
+
+}
---
+++ MovieDatabase/MovieDatabaseTestProject/MovieUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieDatabase.Models;
+using System.Collections.Generic;
+
+namespace MovieDatabaseTestProject
+{
+    [TestClass]
+    public class MovieUnitTest
+    {
+        // test movie
+        private Movie m;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            m = new Movie()
+            {
+                // initialise your test movie attributes
+            };
+        }
+
+        [TestMethod]
+        public void TestCreateMovie()
+        {
+            // verify the movie was created by checking that each attribute value was set
+            // e.g.   Assert.AreEqual(m.Title, "The title of the test movie");
+  
+        }
+
+        [TestMethod]
+        public void TestUpdateMovie()
+        {
+            // make changes to the test movie m            
+          
+            // Check if changes were accepted
+        
+        }
+    }
+}
