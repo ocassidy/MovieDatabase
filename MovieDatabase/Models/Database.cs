2c6f3d5f0f8324007446f068d3817665f61ea8c1
ocassidy <oisin.c@hotmail.co.uk>
2017-11-15 20:05:50 +0000

    Added movie.cs body

--- MovieDatabase/Models/Database.cs
+++ MovieDatabase/Models/Database.cs
@@ -1,7 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace MovieDatabase.Models
@@ -14,79 +11,138 @@ namespace MovieDatabase.Models
         // initialise the database properties
         public Database()
         {
-            
+            _index = 0;
+            db.Clear();
         }
 
         // A property to Return number of movies in the database
-        public int Count
+        public int Count()
         {
-            
+            return db.Count;
         }
 
         // A property to return  current _index position which should be either
         // -1 if database is empty
         // 0 - db.Count-1 if database is not empty
-        public int Index
+        public int Index()
         {
-
+            if (Count() == 0)
+            {
+                _index = -1;
+                
+            }
+            else
+            {
+                _index = (0 - (db.Count - 1));
+            }
+            return _index;
         }
 
         // Add a movie to current position in database
         public void Add(Movie m)
         {
-
+            db.Insert(_index, m);
         }
 
         // Return current movie or null if database empty
         public Movie Get()
         {
-
+            if (Count() == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return db[_index];
+            }
         }
 
         // Delete current movie at index if there is a movie and update index 
         public void Delete()
         {
+            if (Count() != 0)
+            {
+                db.RemoveAt(_index);
+                Index();
 
+            }
         }
 
         // Update the current movie at index if there is a movie and update index
         public void Update(Movie m)
         {
-
+            if (Count() != 0)
+            {
+                db[_index] = m;
+                Index();
+            }
         }
 
         // Delete all movies from the database and reset index
-        public void clear()
+        public void Clear()
         {
-
+            db.Clear();
+            Index();
         }
 
         // Move index position to first movie (0)
         // return true if index update was possible, false otherwise
         public bool First()
         {
-
+            _index = 0;
+            if (_index == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         // Move index position to last movie
         // true if index update was possible, false otherwise</returns>
         public bool Last()
         {
-
+            _index = db.Count - 1;
+            if (_index == db.Count - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         // Move index position to next movie
         // true if index update was possible, false otherwise<
         public bool Next()
         {
-
+            _index++;
+            if (_index < db.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         // Move index position to previous movie
         // true if index update was possible, false otherwise
         public bool Prev()
         {
-
+            _index--;
+            if (_index > -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         // Load movies from a json file and set index to first record
@@ -106,19 +162,19 @@ namespace MovieDatabase.Models
         // order the database by year of movie
         public void OrderByYear()
         {
-
+            db = (from e in db orderby e.GetYear select e).ToList();
         }
 
         // order the database by title of movie (ascending)
         public void OrderByTitle()
         {
-
+            db = (from e in db orderby e.GetTitle select e).ToList();
         }
 
         // order the database by budget of movie (ascending)
         public void OrderByBudget()
         {
-
+            db = (from e in db orderby e.GetBudget select e).ToList();
         }
 
     }
--- MovieDatabase/Models/Movie.cs
+++ MovieDatabase/Models/Movie.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 
 namespace MovieDatabase.Models
 {
-    
+    //public enum Movies {Title, Year, Director, Duration, Budget, Rating, Poster, Actor};
+    public enum Genres { Comedy, Drama, Action, Fiction, Horror, Romance, SciFi, Western, Family };
+
     public class Movie
     {
-        // declare the movie properties and a constructor to initialise a blank movie
-    
-    }
+        private List<string> Actor { get; set; } // list of the actors in the movies
+        private List<Genres> Genre { get; set; } // list of the genres movies can occupy
 
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public int Duration { get; set; }
+        public double Budget { get; set; }
+        public int Rating { get; set; }
+        public string Poster { get; set; }
+        // public string Actor { get; set; }
+
+        public int GetYear
+        {
+            get
+            {
+                return Year;
+            }
+        
+        }
+        public string GetTitle
+        {
+            get
+            {
+                return Title;
+            }
+        }
+        public double GetBudget
+        {
+            get
+            {
+                return Budget;
+            }
+        }
+    }
 }
